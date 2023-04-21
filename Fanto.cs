2f2c304a1e5153f1eb70a4fffa265c1e645752ec
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-21 22:11:48 +0700

    Add files via upload

---
+++ Fanto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    internal class Fanto
+    {
+        public const int SLOW = 1;
+        public const int MEDIUMW = 2;
+        public const int FAST = 3;
+        private int speed = SLOW;
+        public int Speed { get => speed; set => speed = value; }
+        private bool on = false;
+        public bool On { get => on; set => on = value; }
+        private double radius = 5;
+        public double Radius { get => radius; set => radius = value; }
+        private string color = "blue";
+        public string Color { get => color; set => color = value; }
+
+        
+        public Fanto() 
+        {
+            
+        }
+        public Fanto(bool On, int Speed, double Radius, string Color)
+        {
+            this.Speed = Speed;
+            this.On = On;
+            this.Radius = Radius;
+            this.Color = Color;
+        }
+        public virtual void ToString()
+        {
+            if (On == false)
+            { Console.WriteLine($"fan is off:Speed={this.Speed=Speed},Radius={this.Radius = Radius},Color={this.Color = Color}");}
+            if (On == true)
+            { Console.WriteLine($"fan is on:Speed={this.Speed = Speed},Radius={this.Radius = Radius},Color={this.Color = Color}") ; }
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fanto fanto1 = new Fanto(true,3,10, "yellow");
+            fanto1.ToString();
+            Fanto fanto2 = new Fanto(false, 2, 5, "blue");
+            fanto2.ToString();
+            Console.ReadLine();
+        }
+    }
+}
